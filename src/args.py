50b454da66db7b2441628b0ce35aee4166fd10c8
darenlarson <larson.daren@gmail.com>
2019-03-19 15:06:27 -0500

    through scopes

--- src/args.py
+++ src/args.py
@@ -3,8 +3,6 @@
 
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
-
-# YOUR CODE HERE
 def f1(num1, num2):
     return num1 + num2
 
@@ -12,8 +10,6 @@ print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
-
-# YOUR CODE HERE
 def f2(*args):
     sum = 0
     for i in args:
@@ -33,38 +29,45 @@ print(f2(*a))    # Should print 22
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
+def f3(num1, num2 = 1):
+    return num1 + num2
 
-# YOUR CODE HERE
-
-# print(f3(1, 2))  # Should print 3
-# print(f3(8))     # Should print 9
+print(f3(1, 2))  # Should print 3
+print(f3(8))     # Should print 9
 
 
-# # Write a function f4 that accepts an arbitrary number of keyword arguments and
-# # prints out the keys and values like so:
-# #
-# # key: foo, value: bar
-# # key: baz, value: 12
-# #
-# # Google "python keyword arguments".
+# Write a function f4 that accepts an arbitrary number of keyword arguments and
+# prints out the keys and values like so:
+#
+# key: foo, value: bar
+# key: baz, value: 12
+#
+# Google "python keyword arguments".
+# def f4(param, value):
+#     print("{}, {}".format(param, value))
 
-# # YOUR CODE HERE
+# f4(1, 32432)
 
-# # Should print
-# # key: a, value: 12
-# # key: b, value: 30
-# f4(a=12, b=30)
+def f4(**kwargs):
+    for key in kwargs:
+        print("key: {}, value: {}".format(key, kwargs[key]))
 
-# # Should print
-# # key: city, value: Berkeley
-# # key: population, value: 121240
-# # key: founded, value: "March 23, 1868"
-# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-# d = {
-#     "monster": "goblin",
-#     "hp": 3
-# }
+# Should print
+# key: a, value: 12
+# key: b, value: 30
+f4(a=12, b=30)
 
-# # What thing do you have to add to make this work?
-# f4(d)
+# Should print
+# key: city, value: Berkeley
+# key: population, value: 121240
+# key: founded, value: "March 23, 1868"
+f4(city="Berkeley", population=121240, founded="March 23, 1868")
+
+d = {
+    "monster": "goblin",
+    "hp": 3
+}
+
+# What thing do you have to add to make this work?
+f4(**d)
